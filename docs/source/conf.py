c4bfafba00e01c3fd6efc4acf498d7ffd35ad570
Saket Choudhary <saketkc@gmail.com>
2025-05-28 10:26:15 +0530

    Add docs

---
+++ docs/source/conf.py
@@ -0,0 +1,52 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+project = 'imdfetch documentation'
+copyright = '2025, Saket Choudhary'
+author = 'Saket Choudhary'
+
+version = '0.1'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'myst_nb',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+]
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    "tasklist",
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,12 @@
+.. imdfetch Docs documentation master file, created by
+   sphinx-quickstart on Wed May 28 09:34:55 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+imdfetch
+========
+
+.. toctree::
+   :maxdepth: 2
+
+   notebooks/demo.ipynb
