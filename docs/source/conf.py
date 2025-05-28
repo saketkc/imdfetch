3601e2b31ca1edec58217d611b461cbd89dcb96a
Saket Choudhary <saketkc@gmail.com>
2025-05-28 12:00:34 +0530

    Format documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-project = 'imdfetch documentation'
+project = 'imdfetch'
 copyright = '2025, Saket Choudhary'
 author = 'Saket Choudhary'
 
